a101c74b4cdc1462d0e44348f7451896684a87db
MirsalSaidu <140703508+MirsalSaidu@users.noreply.github.com>
2025-04-28 18:57:05 +0400

    Add database test endpoint and fix server config

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(apiRoutes); // This registers all routes from api.js
 
 // Set headers for CORS
 app.use((req, res, next) => {
