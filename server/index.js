1284497612d82be389bae11aec487f1872617de5
MirsalSaidu <140703508+MirsalSaidu@users.noreply.github.com>
2025-04-28 07:56:02 +0400

    Add database test endpoint and fix server config

--- server/index.js
+++ server/index.js
@@ -22,12 +22,32 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../src')));
-app.use(express.static(path.join(__dirname, '..')));
+app.use('/src', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  express.static(path.join(__dirname, '../src'))(req, res, next);
+});
+
+// For other static files
+app.use('/', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  express.static(path.join(__dirname, '..'))(req, res, next);
+});
+
 app.use(heatmapApi);
 
 app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, '../dashboard.html'));
+});
+
+// Add this specific route for heatmap.js
+app.get('/heatmap.js', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(path.join(__dirname, '../src/heatmap.js'));
 });
 
 // For local development
